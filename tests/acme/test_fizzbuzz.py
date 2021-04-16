d27b9e0a9e29133026ed2b53cc023b24d1eab057
Takuto Wada <takuto.wada@gmail.com>
2021-04-16 12:37:05 +0900

    refactor: テストの準備部分が重複したので fixture に抽出した

--- tests/acme/test_fizzbuzz.py
+++ tests/acme/test_fizzbuzz.py
@@ -1,15 +1,17 @@
 from acme.fizzbuzz import FizzBuzz
+import pytest
 
 class TestFizzBuzz:
 
-    def test_1を渡すと文字列1を返す(self):
-        # 準備
-        fizzbuzz = FizzBuzz()
-        # 実行 & 検証
+    @pytest.fixture()
+    def fizzbuzz(self):
+        return FizzBuzz()
+
+    def test_1を渡すと文字列1を返す(self, fizzbuzz):
         assert fizzbuzz.convert(1) == '1'
 
-    def test_2を渡すと文字列2を返す(self):
-        # 準備
-        fizzbuzz = FizzBuzz()
-        # 実行 & 検証
+    def test_2を渡すと文字列2を返す(self, fizzbuzz):
         assert fizzbuzz.convert(2) == '2'
+
+    def test_3を渡すと文字列Fizzを返す(self, fizzbuzz):
+        assert fizzbuzz.convert(3) == 'Fizz'
